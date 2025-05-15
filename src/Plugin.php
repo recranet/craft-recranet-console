506ddfae8914147531999ee73d86f98fb0a4ed0a
Mike Pagé <mikepage@users.noreply.github.com>
2025-05-16 00:16:44 +0200

    Update webhook

--- src/Plugin.php
+++ src/Plugin.php
@@ -36,38 +36,31 @@ class Plugin extends BasePlugin
                 return;
             }
 
-            $this->webhook($user);
+            $webhookUrl = App::env('RECRANET_CONSOLE_CRAFTCMS_VERSION_WEBHOOK');
+
+            if (!$webhookUrl) {
+                Craft::error('Webhook URL not set in environment variable.', __METHOD__);
+                return;
+            }
+
+            $client = Craft::createGuzzleClient([
+                'connect_timeout' => 5,
+            ]);
+
+            $response = $client->post($webhookUrl, [
+                'json' => [
+                    'username' => $user->username,
+                    'datetime' => date('Y-m-d H:i:s'),
+                    'cmsVersion' => Craft::$app->getVersion(),
+                    'cmsLicenseKey' => App::licenseKey(),
+                ]
+            ]);
+
+            if ($response->getStatusCode() === 200) {
+                Craft::info('Webhook sent successfully!', __METHOD__);
+            } else {
+                Craft::error('Error sending webhook: ' . $response->getStatusCode(), __METHOD__);
+            }
         });
-    }
-
-    private function webhook(User $user): void
-    {
-        if (version_compare(Craft::$app->getVersion(), '5.0.0', '>=')) {
-            $webhookUrl = Craft::$app->getConfig()->get('env')['RECRANET_CONSOLE_CRAFTCMS_VERSION_WEBHOOK'] ?? null;
-        } else {
-            $webhookUrl = Craft::getEnv('RECRANET_CONSOLE_CRAFTCMS_VERSION_WEBHOOK');
-        }
-
-        if (!$webhookUrl) {
-            Craft::error('Webhook URL not set in environment variable.', __METHOD__);
-            return;
-        }
-
-        $httpService = Craft::$app->getHttp();
-
-        $response = $httpService->post($webhookUrl, [
-            'json' => [
-                'username' => $user->username,
-                'datetime' => date('Y-m-d H:i:s'),
-                'cmsVersion' => Craft::$app->getVersion(),
-                'cmsLicenseKey' => App::licenseKey(),
-            ]
-        ]);
-
-        if ($response->isOk) {
-            Craft::info('Webhook sent successfully!', __METHOD__);
-        } else {
-            Craft::error('Error sending webhook: ' . $response->statusCode, __METHOD__);
-        }
     }
 }
